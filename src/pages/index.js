452f497ad00896b8750be49c82e25758a4d73304
Stackbit Code Editor <projects@stackbit.com>
2021-06-09 05:11:14 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,7 +53,7 @@ const IndexPage = () => (
             </span>
           </li>
           <li>
-            <span className="icon shield major style2">
+            <span className="icon fa-shield major style2">
               <span className="label">Ipsum</span>
             </span>
           </li>
