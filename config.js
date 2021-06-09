aa45eb3a2e267bfa78d73f444826cb5bd95fe11c
Stackbit Code Editor <projects@stackbit.com>
2021-06-09 04:53:21 +0000

    config.js: changed

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Daniel Vaca',
-  subHeading: 'BackEnd Developer. Part time Open source contributor  ',
+  subHeading: 'BackEnd Developer',
   // social
   socialLinks: [
     {
